2193c039bfbb4d20d4fcc11da9cafecb947f8145
iuliiakrapivin <julia-spb95@yandex.ru>
2023-08-06 17:38:29 +0300

    final commit

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
 from datamanager.json_data_manager import JSONDataManager
 
+
 app = Flask(__name__)
 data_manager = JSONDataManager('datamanager/users_data.json')
 
 
 @app.route('/users')
 def list_users():
+    """Route renders main page template with all users"""
     users = data_manager.get_all_users()
     return render_template('users.html', users=users)
 
 
 @app.route('/users/<user_id>')
 def list_user_movies(user_id):
-    movies_list = data_manager.get_user_movies(user_id)
-    return render_template('movies.html', user=movies_list, user_id=user_id)
+    """Route renders page template with all movies for chosen user"""
+    try:
+        movies_list = data_manager.get_user_movies(user_id)
+        return render_template('movies.html', user=movies_list, user_id=user_id)
+    except TypeError:
+        return "User with this id doesn't exist"
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
-
+    """Route renders page template with form for adding new user"""
     if request.method == 'POST':
         user_name = request.form.get('name')
+        if user_name == '':
+            return "User name is empty"
         data_manager.add_user(user_name)
         return redirect(url_for('list_users'))
     return render_template('add_user.html')
@@ -29,15 +37,57 @@ def add_user():
 
 @app.route('/users/<user_id>/add_movie', methods=['GET', 'POST'])
 def add_movie(user_id):
+    """Route renders page template with form for adding new movie for chosen user"""
+    try:
+        if request.method == 'POST':
+            name = request.form.get('name')
+            if name == '':
+                return "Movie name is empty"
+            data_manager.add_movie(user_id, name)
+            return redirect(url_for('list_user_movies', user_id=user_id))
+        return render_template('add_movie.html', user_id=user_id)
+    except TypeError:
+        return "User with this id doesn't exist"
 
-    if request.method == 'POST':
-        name = request.form.get('name')
-        director = request.form.get('director')
-        year = request.form.get('year')
-        rating = request.form.get('rating')
-        data_manager.add_movie(user_id, name, director, year, rating)
+
+@app.route('/users/<user_id>/update_movie/<int:movie_id>', methods=['GET', 'POST'])
+def update_movie(user_id, movie_id):
+    """Route renders page template with form for updating chosen movie for chosen user"""
+    try:
+        user_movies = data_manager.get_user_movies(user_id)  # returns all user movies
+        for movie in user_movies:
+            if movie['id'] == movie_id:  # searching for exact movie by provided id
+                if request.method == 'POST':  # update the movie data if new data sent
+                    name = request.form.get('name')
+                    director = request.form.get('director')
+                    year = request.form.get('year')
+                    rating = request.form.get('rating')
+                    data_manager.update_movie(user_id, movie_id, name, director, year, rating)
+                    return redirect(url_for('list_user_movies', user_id=user_id))
+                return render_template('update_movie.html', movie=movie, movie_id=movie_id, user_id=user_id)
+        return "Movie with this id doesn't exist"
+    except TypeError:
+        return "User with this id doesn't exist"
+
+
+@app.route('/users/<user_id>/delete_movie/<int:movie_id>')
+def delete_movie(user_id, movie_id):
+    """Route for deleting chosen movie for chosen user"""
+    try:
+        data_manager.delete_movie(user_id, movie_id)
         return redirect(url_for('list_user_movies', user_id=user_id))
-    return render_template('add_movie.html', user_id=user_id)
+    except TypeError:
+        return "User or movie with this id doesn't exist"
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return render_template('405.html'), 405
 
 
 if __name__ == '__main__':
--- datamanager/json_data_manager.py
+++ datamanager/json_data_manager.py
@@ -1,17 +1,26 @@
 import json
+import requests
 from datamanager.data_manager_interface import DataManagerInterface
+
+KEY = '34571e1b'
 
 
 class JSONDataManager(DataManagerInterface):
+    """Class created to perform operations with users data stored in JSON file"""
     def __init__(self, filename):
         self.filename = filename
 
     def get_all_users(self):
-        with open(self.filename, "r") as fileobject:
-            users_list = json.loads(fileobject.read())
-            return users_list
+        """Function uploads all users from JSON file and returns it as a list"""
+        try:
+            with open(self.filename, "r") as fileobject:
+                users_list = json.loads(fileobject.read())
+                return users_list
+        except IOError as e:
+            print("An IOError occurred: ", str(e))
 
     def get_user_movies(self, user_id):
+        """Function uploads all user movies from JSON file and returns it as a list"""
         users_list = self.get_all_users()
         for user in users_list:
             for key in user.keys():
@@ -20,43 +29,80 @@ class JSONDataManager(DataManagerInterface):
                     return user[f'{user_id}']['movies']
 
     def add_user(self, user_name):
+        """Function takes new username as parameter, creates
+           and adds a new user dictionary to the JSON file"""
         users_list = self.get_all_users()
         id_list = [int(us_id) for user in users_list for us_id in user.keys()]
         try:
             user_id = max(id_list) + 1  # assigns a unique id to new user
         except ValueError:
-            user_id = 1
+            user_id = 1  # if id list is empty will be assigned 1
         new_user = {f"{user_id}": {"name": user_name, "movies": []}}
         users_list.append(new_user)
+        json_str = json.dumps(users_list)  # write updated users list to the file
+        with open(self.filename, "w") as new_file_object:
+            new_file_object.write(json_str)
+
+    def update_movies_list(self, user_id, changed_movie_list):
+        """Function rewrites updated movie list for given user to the file"""
+        users_list = self.get_all_users()
+        for user in users_list:
+            for key in user.keys():
+                if key == user_id:
+                    user[f'{user_id}']['movies'] = changed_movie_list
         json_str = json.dumps(users_list)
         with open(self.filename, "w") as new_file_object:
             new_file_object.write(json_str)
 
-    def add_movie(self, user_id, name, director, year, rating):
+    def add_movie(self, user_id, name):
+        """Function takes new movie name and user id as parameters, searches for movie by sending request
+        to the API, takes movie data from response and adds a new movie data to exact user movies list"""
         user_movies = self.get_user_movies(user_id)
         id_list = [int(val) for movie in user_movies for key, val in movie.items() if key == "id"]
         try:
             movie_id = max(id_list) + 1  # assigns a unique id to new movie
         except ValueError:
-            movie_id = 1
-        new_movie = {'id': movie_id, 'name': name, 'director': director, 'year': year, 'rating': rating}
-        user_movies.append(new_movie)
+            movie_id = 1  # if id list is empty will be assigned 1
+        # making a request with movie name
+        try:
+            url = f'https://www.omdbapi.com/?apikey={KEY}&t={name}'
+            response = requests.get(url)
+            data = response.json()  # saves the response
+            # filters the response
+            name = data['Title']
+            year = data['Year']
+            rating = float(data['Ratings'][0]['Value'][:-3])
+            director = data['Director']
+            # creating a new movie record
+            new_movie = {'id': movie_id, 'name': name, 'director': director, 'year': year, 'rating': rating}
+            user_movies.append(new_movie)
+        except KeyError:
+            print("Movie is not found")  # prints an error message if movie wasn't found
+        except requests.exceptions.ConnectionError:
+            print("Connection issue")  # prints an error message if connection problem is appeared
+        except IndexError:  # prints an error message if rating format is in unexpected format
+            print("Rating format not supported")
+        # writes updated movie list to the storage
+        self.update_movies_list(user_id, user_movies)
 
-        users_list = self.get_all_users()
-        for user in users_list:
-            print(user)
-            for key in user.keys():
-                print(key)
-                print(user_id)
-                if key == user_id:
-                    print(user[f'{user_id}']['movies'])
-                    user[f'{user_id}']['movies'] = user_movies
-        json_str = json.dumps(users_list)
-        with open(self.filename, "w") as new_file_object:
-            new_file_object.write(json_str)
+    def update_movie(self, user_id, movie_id, name, director, year, rating):
+        """Function takes exact movie data and user id as parameters, searches for movie in the storage,
+        updates and saves movie record with provided data"""
+        user_movies = self.get_user_movies(user_id)
+        for movie in user_movies:
+            if movie['id'] == movie_id:  # searching for exact movie by provided id
+                movie['name'] = name
+                movie['director'] = director
+                movie['year'] = year
+                movie['rating'] = rating
+        # writes updated movie list to the storage
+        self.update_movies_list(user_id, user_movies)
 
-
-
-
-
-
+    def delete_movie(self, user_id, movie_id):
+        """Function takes movie id and user id as parameters to find and delete exact movie from the storage"""
+        user_movies = self.get_user_movies(user_id)
+        for movie in user_movies:
+            if movie['id'] == movie_id:  # searching for exact movie by provided id
+                user_movies.remove(movie)
+        # writes updated movie list to the storage
+        self.update_movies_list(user_id, user_movies)
